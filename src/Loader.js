c2fe69663201d2142e23c96a24b51f9bc49b1c1d
Darren <darrenware33@gmail.com>
2018-06-18 21:32:02 +1000

    Adjusted formatting

--- src/Loader.js
+++ src/Loader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Loader = () => {
+const Loader = () => { // loading animation
 
     return (
 		<div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker'; // disable service workers
+import { unregister } from './registerServiceWorker'; // disable service workers because I'm not using https - avoids error
 unregister();
 
 ReactDOM.render(<App />, document.getElementById('root'));
