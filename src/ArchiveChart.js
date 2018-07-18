d8af97df9c863f6dfc8ad33d10d178db3fb1bee0
Darren <darrenware33@gmail.com>
2018-07-18 22:30:46 +1000

    Adjusted props and added calorie counter to component

--- src/ArchiveChart.js
+++ src/ArchiveChart.js
@@ -1,44 +1,44 @@
 import React from 'react';
-import { Chart } from './react-google-charts';
 
-class ArchiveChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      options: {
-          title: 'This Weeks Overview',
-          pieSliceText: 'value',
-          pieSliceTextStyle: {color: 'black', fontSize: 14},
-          is3D: true,
-          colors:['#49eadb','#49c0ea', '#5f49ea', '#b049ea', '#49ea8e','#c5ea49','#ea9849']
-      },
-      data: [
-        ['Day', 'Calories'],
-        ['Mon', 50],
-        ['Tues', 70],
-        ['Weds', 90],
-        ['Thurs', 20],
-        ['Fri', 10],
-        ['Sat', 10],
-        ['Sun', 60]
-      ],
-    };
+const ArchiveChart = ({archiveData, archiveKey, chart}) => {
+
+  let Chart = chart;
+  let cal = 0;
+
+  this.state = {
+    options: {
+        title: `The Week Beginning:
+                ${archiveData[archiveKey] === undefined? 'No Data Available!' : archiveData[archiveKey].date}`,
+        pieSliceText: 'value',
+        pieSliceTextStyle: {color: 'black', fontSize: 14},
+        is3D: true,
+        colors:['#49eadb','#49c0ea', '#5f49ea', '#b049ea', '#49ea8e','#c5ea49','#ea9849']
+    }
+  };
+
+  // get the total calories for the archived week
+  if (archiveData[archiveKey] !== undefined) {
+    const l = archiveData[archiveKey].data.length;
+    for (let i = 1; i < l; i++) {
+      let value = archiveData[archiveKey].data[i][1];
+      cal = cal + value;
+    }
   }
 
-  render() {
-
-    return (
+  return (
+    <div>
+      <h3>Calories For Week: <span>{cal}</span></h3>
+      <hr/>
       <Chart
         chartType="PieChart"
-        data={this.state.data}
+        data= {archiveData[archiveKey] === undefined? '' : archiveData[archiveKey].data}
         options={this.state.options}
-        graph_id="PieChart"
         width="100%"
         height="400px"
         legend_toggle
       />
-    );
-  }
+    </div>
+  );
 }
 
 export default ArchiveChart;
